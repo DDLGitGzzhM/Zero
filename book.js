80e7275e475f051d6701875a9cf0c1cceae6f4f8
gzm <gzm@1token.trade>
2024-05-13 13:00:40 +0800

    init

---
+++ book.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // 书籍信息
+    title: "书名",
+    description: "描述",
+    isbn: "图书编号",
+    author: "作者",
+    lang: "zh-cn",
+    // 插件列表
+    plugins: ["-lunr","-search","search-pro","code","expandable-chapters","back-to-top-button"],
+    // 插件全局配置
+    pluginsConfig: {},
+    // 模板变量
+    variables:{
+        // 自定义
+    }
+}
